4017363164eb0481cdb28d9ae6ce48c543caac32
CronYCranKy <ronnygurski@hotmail.de>
2024-09-09 10:51:56 +0000

    GET  befehl geändert und test html doc erstellt

--- index.js
+++ index.js
@@ -14,10 +14,20 @@ app.listen(
 
 // Endpoint GET
 app.get('/' , (req,res) => {
-    res.status(200).send({
-        product: "T-Shirt",
-        size: "L",
-        prize: 10.00,
-        hasLogo: false
-    })
+    res.status(200).send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meine Seite</title>
+</head>
+<body>
+    <h1>Cool</h1>
+    <p>das ist eine coole sache!</p>
+</body>
+</html>`
+
+
+
+    )
 });
